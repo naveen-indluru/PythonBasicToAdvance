3d2ae4f3ac3b798e5e813f75067ba6049407ba41
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 22:05:29 +0530

    Python simple casting

---
+++ python_simple_casting.py
@@ -0,0 +1,18 @@
+# program to understand the casting in python
+
+'''
+If you want to specify the data type of a variable, this can be done with casting
+
+below is the example
+'''
+
+x=str(3)        # x will be '3'
+y=int(3)        # y will be  3
+z=float(3)      # z will be  3.0
+
+
+
+#dispalying x,y and z values
+print(x)
+print(y)
+print(z)
