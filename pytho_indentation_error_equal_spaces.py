81b3c16e6ca9c89faea438be81320c6f3dd4e575
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 16:31:43 +0530

    Python indentation error, equal spaces with in a single block

---
+++ pytho_indentation_error_equal_spaces.py
@@ -0,0 +1,21 @@
+#program to understand python indentation
+
+'''
+The number of spaces is upto you as a programmer, but it has to be at least one.
+
+if 5>2:
+    print("Five is grater than two!!") #one space
+if 7>2:
+      print("Seven is grater than two!!") #Four Spaces
+'''
+
+
+'''
+You have to use same number of spaces in the same block of code, other wise python will give you an error
+
+'''
+
+if 5>2:
+    print("Five is grater than two!!") #one space
+        print("Seven is grater than two!!") #Four Spaces, so execution leads to indentation error
+  
