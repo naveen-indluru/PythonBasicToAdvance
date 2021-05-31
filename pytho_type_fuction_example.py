15a0802f4d2e910c406bde585d1a7935bccc5eb0
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 22:12:57 +0530

    python type() fuction example

---
+++ pytho_type_fuction_example.py
@@ -0,0 +1,14 @@
+#program to undetstand python "type()" function.
+
+'''
+You can get the type of the variable with a type function.
+'''
+
+x=3
+y='Tony'
+
+
+#Dispay the type of the variables x and y
+print(type(x))    # will print type of variable x
+
+print(type(y))    # will print type of variable y
