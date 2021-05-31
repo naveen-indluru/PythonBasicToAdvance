511c15bd41cd5feaec1d9f526bd5bda6dc1ece34
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 21:56:36 +0530

    Variables in python

---
+++ python_varibles.py
@@ -0,0 +1,24 @@
+#program to understand python variables
+
+"""
+variables are contaiers for storing a data value
+Python has no command for declaring variables
+A variable is created when you first assign a value to it.
+"""
+
+x=7
+y='Steve'
+print(x)
+print(y)
+
+
+print("below is the 2nd program output")
+
+'''
+In python variables do not need to be declared with any particular type,
+and can even change the type after they have been set.
+'''
+
+x=4         # x is of type int
+x='Peggy'   # x is of type str
+print(x)
