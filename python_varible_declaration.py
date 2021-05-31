4fc37d2f5ad1004ec8669a7f40f88313c1ba1e51
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 16:42:31 +0530

    simple python variable declaration

---
+++ python_varible_declaration.py
@@ -0,0 +1,19 @@
+#Program to understand variable creation in python
+
+'''
+In python variables are created when you assign a value to it
+'''
+
+x=5        #the variable x is created, as we have assigned a value to it.
+y="Naveen" #the variable y is created, as we have assigned a value to it.
+
+
+print(x) #prints value which is stored in the variable 'x'
+
+print(y) #prints the value which is stored in the variable 'y'.
+
+
+
+print(type(x)) #prints the data type of variable 'x'.
+
+print(type(y)) # prints the data type of variable 'y'.
