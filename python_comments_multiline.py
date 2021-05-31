c43d2c9bb7fdd10d6dc38f492d95a50b1a08c3e0
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 20:46:02 +0530

    Python multi-line comment in detail

---
+++ python_comments_multiline.py
@@ -0,0 +1,27 @@
+#program to understand the multi-line comments in python
+
+'''
+Since python will ignore string literals which are not assigned to a variable,you can add a multi-;ine string (Triple quotes) in your code,
+and place your comments inside it.
+'''
+
+'''
+As long as the string is not assigned to a variable, python read the code, but later ignore it,
+and you have made a multiline comment.
+'''
+
+'''
+
+This is a comment
+written in
+more than one just one line
+'''
+
+
+"""
+This is comment
+written in
+more than just one line
+"""
+
+print("Hey!!, you have made a multi-line comment in above line of code")
