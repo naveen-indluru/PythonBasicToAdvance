d894be23b6cebf36b73291eae4e40ee6b181db62
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 15:47:44 +0530

    Python Indentation

---
+++ pytho_indentation.py
@@ -0,0 +1,12 @@
+#Program to understand python indentation
+
+'''
+Indentation refers to the spaces at the begining of a code line.
+where in other programming languages the indentation in code is for readability.
+In python Indentation is very important.
+Python uses indentation to indicate a block of code.
+   
+'''
+
+if 7>2:
+    print ("Seven is grater!! than two") #the white space after at the begining of the print statement refers to the indentation.
