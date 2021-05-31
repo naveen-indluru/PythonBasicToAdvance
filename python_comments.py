96c19eef5f5ceefaeed8512587aff68514f3751c
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 20:27:47 +0530

    comments in python

---
+++ python_comments.py
@@ -0,0 +1,9 @@
+#Program to understand python comments
+
+'''
+Python has commenting capability for the purpose of incode documentation.
+Comments start with a #, and python renders the rest of the line as commet.
+'''
+
+#This is a comment
+print("This is python single line comment..")
