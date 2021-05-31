e9eadc2cd38ced0ab84b2fcfcfd7120fb1f93df0
inaveen7 <naveenindluru.dev@gmail.com>
2021-05-31 16:09:38 +0530

    Python Indentation Error.

---
+++ pytho_indentation_error.py
@@ -0,0 +1,9 @@
+#Program to understand python indentation error
+'''
+Python will give you an error if you skip the indentation.
+Below is the example for python indentation error
+'''
+
+if 7>5:
+print ("Seven is grater than Five") #Since there is no space at the begining of print statement python will give indentation error.    
+    
