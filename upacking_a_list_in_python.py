b200f085651cbd466043dbb2e2548eaf5c254a14
inaveen7 <naveenindluru.dev@gmail.com>
2021-06-01 22:12:10 +0530

    Unpacking a collection [a list] in python. just an intro to unpacking a collection in python

---
+++ upacking_a_list_in_python.py
@@ -0,0 +1,19 @@
+# program to understand unpack a collection in python
+
+'''
+If you have a collection of variables in a List, Tuple, etc..
+Python allows you to extract the values into variables.
+This is called UnPacking.
+'''
+
+# Unpacking a List
+
+friuts =['Mango','Apple','Banana']
+
+y,x,z = friuts
+
+# Displaying collection after unpacking into variables
+
+print(x)
+print(y)
+print(z)
